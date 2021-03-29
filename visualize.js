6c5524064c4573c8146a35daa745105e4ea0a571
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-29 00:21:11 -0400

    Add visualize.js

---
+++ visualize.js
@@ -0,0 +1,12 @@
+/**
+ * Visualizes a set of table data
+ * @param  {{headers: Array, values: Array}} data
+ *         An object containing the headers and values of a table
+ * @return {string}
+ *         A string representation of the table data
+ */
+const visualize = (data) => {
+
+
+
+}
